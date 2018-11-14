bea31ad5a61c5704801b65b1d46f7b5d08118b8a
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-11-14 19:34:32 -0200

    Model: using random_state variable in order to always get the same results.

--- face_detector/model.py
+++ face_detector/model.py
@@ -24,32 +24,30 @@ class Model():
         y = np.array(y)
         nsamples, nx, ny = X.shape
         X = X.reshape((nsamples, nx*ny))
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
 
-        #  https://pythonmachinelearning.pro/face-recognition-with-eigenfaces/
         n_components = 100
-        pca = PCA(n_components=n_components, whiten=True).fit(X_train)
+        pca = PCA(n_components=n_components,random_state=42, whiten=True).fit(X_train)
         X_train_pca = pca.transform(X_train)
         X_test_pca = pca.transform(X_test)
 
         print("SVM: ")
-        clf = SVC()
+        clf = SVC(random_state=42)
         clf.fit(X_train_pca, y_train)
-        #clf = MLPClassifier(hidden_layer_sizes=(1024,), solver='lbfgs', batch_size=256).fit(X_train_pca, y_train)
         y_pred = clf.predict(X_test_pca)
-        print(classification_report(y_test, y_pred, target_names=target_names))
+        print(classification_report(y_test, y_pred, target_names=target_names, labels=np.array(target_names)))
         print()
 
         print("LDA: ")
         clf = LinearDiscriminantAnalysis()
         clf.fit(X_train_pca, y_train)
         y_pred = clf.predict(X_test_pca)
-        print(classification_report(y_test, y_pred, target_names=target_names))
+        print(classification_report(y_test, y_pred, target_names=target_names, labels=np.array(target_names)))
         print()
 
         print("Naive Bayes: ")
         clf = GaussianNB()
         clf.fit(X_train_pca, y_train)
         y_pred = clf.predict(X_test_pca)
-        print(classification_report(y_test, y_pred, target_names=target_names))
+        print(classification_report(y_test, y_pred, target_names=target_names, labels=np.array(target_names)))
         print()
